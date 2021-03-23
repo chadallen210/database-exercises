1c1a51a6e5c1485f1ed39d65ffe2c41a2ad71a2e
Chad Allen <chad.allen.210@gmail.com>
2021-03-23 16:14:47 -0500

    add tables exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,10 @@
+use employees;
+show tables;
+
+## 5. INT, DATE, VARCHAR, ENUM
+## 6. dept_emp, dept_manager, employees, salaries, titles
+## 7. departments, dept_emp, dept_manager, employees, titles
+## 8. dept_emp, dept_manager, employees, salaries, titles
+## 9. doesn't show any relationship
+
+show create table dept_manager;
