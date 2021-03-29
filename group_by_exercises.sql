2dac18b73ea7d772d637eb82161f8df4af468eaf
Chad Allen <chad.allen.210@gmail.com>
2021-03-29 15:58:43 -0500

    addint group by exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,61 @@
+-- Group By Exercises
+
+-- 1. Create a new file named group_by_exercises.sql
+USE employees;
+
+-- 2. In your script, use DISTINCT to find the unique titles in the titles table. How many unique titles have there ever been? Answer that in a comment in your SQL file.
+
+SELECT DISTINCT title
+FROM titles;
+-- returns 7 records
+
+-- 3. Write a query to find a list of all unique last names of all employees that start and end with 'E' using GROUP BY.
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name;
+-- returns 5 records
+
+-- 4. Write a query to find all unique combinations of first and last names of all employees whose last names start and end with 'E'.
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY first_name, last_name;
+-- returns 846 records
+
+-- 5. Write a query to find the unique last names with a 'q' but not 'qu'. Include those names in a comment in your sql code.
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+-- returns 3 records - Chleq, Lindqvist, Qiwen
+
+-- 6. Add a COUNT() to your results (the query above) and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+
+SELECT last_name, count(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY count(last_name) DESC;
+
+-- 7. Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
+
+SELECT first_name, gender, count(gender)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY first_name, gender
+ORDER BY count(gender);
+
+-- 8. Using your query that generates a username for all of the employees, generate a count employees for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames are there?
+
+SELECT lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4), '_', substr(birth_date, 6, 2), substr(birth_date, 3, 2))) AS username, count(*)
+FROM employees
+GROUP BY username
+HAVING count(username) > 1
+ORDER BY count(username) DESC;
+-- Yes! There are 13251 duplicate usernames.
